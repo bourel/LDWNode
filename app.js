0c5763729e67b41d586a9cf6747b5a3508be7e88
Guillaume Bourel <guillaume.bourel@ndmac-systems.com>
2014-10-10 09:56:14 +0200

    Question 4

--- app.js
+++ app.js
@@ -10,16 +10,42 @@ server = http.createServer(function (req, res) {
   console.log(req.url);
   // Début de la fonction de callback : 
   // cette fonction est appelé à chaque requête du client
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<!DOCTYPE html>' +
+  if(req.url == "/"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<!DOCTYPE html>' +
 	'<html>' +
 	  '<head>' +
 	    '<title>TP Node</title>' +
 	  '</head>' +
 	  '<body>' +
 	    '<h1>TP Node.js</h1>' +
+	    '<p>Page d\'accueil</p>' +	
 	  '</body>' +
 	'</html>');
+  } else if(req.url == "/about"){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<!DOCTYPE html>' +
+	'<html>' +
+	  '<head>' +
+	    '<title>TP Node</title>' +
+	  '</head>' +
+	  '<body>' +
+	    '<h1>TP Node.js</h1>' +
+	    '<p>A propos</p>' +
+	  '</body>' +
+	'</html>');
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.end('<!DOCTYPE html>' +
+	'<html>' +
+	  '<head>' +
+	    '<title>TP Node</title>' +
+	  '</head>' +
+	  '<body>' +
+	    '<h2>Argh page non trouvée</h1>' +
+	  '</body>' +
+	'</html>');
+  }
 });
 
 server.listen(PORT);
