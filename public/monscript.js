0b6c3f04055d4fba70760ea115abc91f5111224c
Guillaume Bourel <guillaume.bourel@ndmac-systems.com>
2014-10-10 13:38:08 +0200

    Question 12

--- public/monscript.js
+++ public/monscript.js
@@ -2,7 +2,11 @@
 (function(){
 
  $('#toggleBtn').click(function(){
-   $('#title').slideToggle();
+   $('#title').slideToggle(2000, function(){
+     if($('#title').css('display') == 'none'){
+       console.info("Le titre est masqué");
+     }
+   });
  });
 
 })();
